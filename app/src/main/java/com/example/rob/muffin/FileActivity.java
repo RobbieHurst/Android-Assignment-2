26bd9f1be7b911f771178eb6a364f60dda8f17e3
Robert Hurst <13005890@vcconnect.co.za>
2017-05-25 10:36:14 +0200

    FIle System

--- app/src/main/java/com/example/rob/muffin/FileActivity.java
+++ app/src/main/java/com/example/rob/muffin/FileActivity.java
@@ -3,6 +3,12 @@ package com.example.rob.muffin;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by roberthurst on 24/05/2017.
@@ -16,6 +22,30 @@ public class FileActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.file_layout);
+
+        readFromFile();
+
+    }
+    public void readFromFile(){
+
+        BufferedReader fileRead = null;
+        String textFileData = " ";
+        String line;
+
+        try{
+
+            fileRead = new BufferedReader(new InputStreamReader(getAssets().open("Restaurant")));
+
+            while((line = fileRead.readLine()) != null){
+                textFileData += line + "\n";
+            }
+
+            Toast.makeText(this, textFileData, Toast.LENGTH_LONG).show();
+
+        }
+        catch (IOException error){
+            error.printStackTrace();
+        }
     }
 
 }
